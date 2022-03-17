3b30b2fdc1f804d22a7d136f74b2e12619697fe0
kyunosaya <saya0203@naver.com>
2022-03-17 19:42:15 +0900

    start

---
+++ src/main.js
@@ -0,0 +1,25 @@
+class Book{
+    //constructor:function(){}
+    constructor(type,title){
+        this.type = type;
+        this.title = title;
+    }
+    //list:function(){}
+    list(){
+        document.write(
+            `
+            <p>
+            ${this.type} : ${this.title}
+            </p>
+            `
+        )
+    }
+}
+
+
+const photo = new Book('graphic','photoshop');
+const vector = new Book('graphic','illustrator');
+const code = new Book('it','html');
+photo.list();
+vector.list();
+code.list();
